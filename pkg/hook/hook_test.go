9b501e51a6a7f827c6d7e8f5274f191569cdf76e
Ivan Mikheykin <ivan.mikheykin@flant.com>
2019-04-03 20:28:34 +0300

    test: Hook.SafeName

---
+++ pkg/hook/hook_test.go
@@ -0,0 +1,22 @@
+package hook
+
+import (
+	"testing"
+	"path/filepath"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHook_SafeName(t *testing.T) {
+	WorkingDir = "/hooks"
+	hookPath := "/hooks/002-cool-hooks/monitor-namespaces.py"
+
+	hookName, err := filepath.Rel(WorkingDir, hookPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	h := NewHook(hookName, hookPath)
+
+	assert.Equal(t, "002-cool-hooks-monitor-namespaces-py", h.SafeName())
+}
