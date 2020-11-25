50f46b298da0cddcc6b9cb99a1b01e16efc279e8
Ivan Mikheykin <ivan.mikheykin@flant.com>
2020-11-25 20:02:58 +0300

    ++ fix vault_test

--- pkg/metric_storage/vault/vault_test.go
+++ pkg/metric_storage/vault/vault_test.go
@@ -33,11 +33,7 @@ metric_total{lbl="val"} 1
 	err := promtest.GatherAndCompare(prometheus.DefaultGatherer, strings.NewReader(expect), "metric_total")
 	g.Expect(err).ShouldNot(HaveOccurred())
 
-	v.ClearMissingMetrics("group1", map[string][]map[string]string{
-		"metric_total": {
-			{"lbl": "qwe"},
-		},
-	})
+	v.ExpireGroupMetrics("group1")
 
 	g.Expect(buf.String()).ShouldNot(ContainSubstring("error"), "error occurred in log: %s", buf.String())
 
@@ -74,14 +70,7 @@ metric_total{lbl="val222"} 1
 	err = promtest.GatherAndCompare(prometheus.DefaultGatherer, strings.NewReader(expect), "metric_total")
 	g.Expect(err).ShouldNot(HaveOccurred())
 
-	v.ClearMissingMetrics("group1", map[string][]map[string]string{
-		"metric_total": {
-			{"lbl": "val"},
-		},
-		"metric2_total": {
-			{"lbl222": "val222"},
-		},
-	})
+	v.ExpireGroupMetrics("group1")
 	v.CounterAdd("group1", "metric_total", 1.0, map[string]string{"lbl": "val"})
 	v.CounterAdd("group2", "metric2_total", 1.0, map[string]string{"lbl": "val222"})
 
@@ -98,18 +87,11 @@ metric2_total{lbl="val222"} 1
 	err = promtest.GatherAndCompare(prometheus.DefaultGatherer, strings.NewReader(expect), "metric_total", "metric2_total")
 	g.Expect(err).ShouldNot(HaveOccurred())
 
-	v.ClearMissingMetrics("group1", map[string][]map[string]string{
-		"metric2_total": {
-			{"lbl222": "val222"},
-		},
-	})
+	v.ExpireGroupMetrics("group1")
 	v.CounterAdd("group2", "metric2_total", 1.0, map[string]string{"lbl": "val222"})
 	g.Expect(buf.String()).ShouldNot(ContainSubstring("error"), "error occurred in log: %s", buf.String())
 	// Expect metric_total is updated and metric2_total is updated and metric_total left as is
 	expect = `
-# HELP metric_total metric_total
-# TYPE metric_total counter
-metric_total{lbl="val"} 1
 # HELP metric2_total metric2_total
 # TYPE metric2_total counter
 metric2_total{lbl="val222"} 2
