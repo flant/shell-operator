7349d4c15eb43b4befd49d72ed217edad216e71d
Ivan Mikheykin <ivan.mikheykin@flant.com>
2021-01-12 12:59:34 +0300

    last changes

--- examples/204-validating-webhook/gen-certs.sh
+++ examples/204-validating-webhook/gen-certs.sh
@@ -12,11 +12,9 @@ echo THIS SCRIPT IS NOT SECURE! USE IT ONLY FOR DEMONSTATION PURPOSES.
 echo =================================================================
 echo
 
-mkdir -p validating-certs
+mkdir -p validating-certs && cd validating-certs
 
-cd validating-certs
-
-if [[ -e server-key.pem  ]] ; then
+if [[ -e cert.key  ]] ; then
   read -p "Regenerate certificates? (yes/no) [no]: "
   if [[ ! $REPLY =~ ^[Yy][Ee][Ss]$ ]]
   then
@@ -24,11 +22,12 @@ if [[ -e server-key.pem  ]] ; then
   fi
 fi
 
-echo ">>> Remove server.crt server.csr server-key.pem ca.pem"
-rm -f server.crt server.csr server-key.pem ca.pem
+RM_FILES="cert.crt cert.key cert.csr ca.crt"
+echo ">>> Remove ${RM_FILES}"
+rm -f $RM_FILES
 
-# generate server-key.pem server.csr
-echo ">>> Generate server-key.pem"
+# generate cert.key, cert.csr
+echo ">>> Generate cert.key and cert.csr"
 cat <<EOF | cfssl genkey - | cfssljson -bare server
 {
   "hosts": [
@@ -43,15 +42,19 @@ cat <<EOF | cfssl genkey - | cfssljson -bare server
 }
 EOF
 
+mv server-key.pem cert.key
+mv server.csr cert.csr
+
 echo ">>> Delete previous CertificateSigningRequest"
-(kubectl delete certificatesigningrequest/${CERT_NAME} || true )
+kubectl delete certificatesigningrequest/${CERT_NAME} --ignore-not-found
 
 
 echo ">>> Create CertificateSigningRequest"
 
 # create CertificateSigningRequest resource
 # name is in form serviceName.namespace
-cat <<EOF | kubectl apply -f -
+if [[ ${CSR_V1BETA1} == "yes" ]] ; then
+  cat <<EOF | kubectl apply -f -
 apiVersion: certificates.k8s.io/v1beta1
 kind: CertificateSigningRequest
 metadata:
@@ -59,20 +62,37 @@ metadata:
   labels:
     heritage: example-204
 spec:
-  request: $(cat server.csr | base64 | tr -d '\n')
+  request: $(cat cert.csr | base64 | tr -d '\n')
   usages:
   - digital signature
   - key encipherment
   - server auth
 EOF
+else
+  cat <<EOF | kubectl apply -f -
+apiVersion: certificates.k8s.io/v1
+kind: CertificateSigningRequest
+metadata:
+  name: ${CERT_NAME}
+  labels:
+    heritage: example-204
+spec:
+  request: $(cat cert.csr | base64 | tr -d '\n')
+  signerName: kubernetes.io/kube-apiserver-client
+  usages:
+  - digital signature
+  - key encipherment
+  - server auth
+EOF
+fi
 
 kubectl certificate approve $CERT_NAME
 
 echo ">>> Retrieve server.crt"
-kubectl get certificatesigningrequest $CERT_NAME -o jsonpath='{.status.certificate}' | base64 -d > server.crt
+kubectl get certificatesigningrequest $CERT_NAME -o jsonpath='{.status.certificate}' | base64 -d > cert.crt
 
 echo ">>> Delete CertificateSigningRequest"
 (kubectl delete certificatesigningrequest/${CERT_NAME} || true )
 
-echo ">>> Retrieve ca.pem"
-kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}' | base64 -d > ca.pem
+echo ">>> Retrieve ca.crt"
+kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}' | base64 -d > ca.crt
