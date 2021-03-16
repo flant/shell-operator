ab305a6eb85dd0109156a9212f49e0dc554f39b9
Ivan Mikheykin <ivan.mikheykin@flant.com>
2021-03-16 17:00:17 +0300

    feat: separate kube client for object patcher
    Timeout for 'main' kube client resets Watcher sessions. So, separate kube client with timeout for object patcher is the best what we can do without huge upgrade to 0.20.

--- pkg/app/kube-client.go
+++ pkg/app/kube-client.go
@@ -9,41 +9,56 @@ import (
 var KubeContext = ""
 var KubeConfig = ""
 var KubeServer = ""
+
 var KubeClientQpsDefault = "5" // DefaultQPS from k8s.io/client-go/rest/config.go
 var KubeClientQps float32
 var KubeClientBurstDefault = "10" // DefaultBurst from k8s.io/client-go/rest/config.go
 var KubeClientBurst int
-var KubeClientTimeoutDefault = "10s"
-var KubeClientTimeout time.Duration
+
+var ObjectPatcherKubeClientQpsDefault = "5" // DefaultQPS from k8s.io/client-go/rest/config.go
+var ObjectPatcherKubeClientQps float32
+var ObjectPatcherKubeClientBurstDefault = "10" // DefaultBurst from k8s.io/client-go/rest/config.go
+var ObjectPatcherKubeClientBurst int
+var ObjectPatcherKubeClientTimeoutDefault = "10s"
+var ObjectPatcherKubeClientTimeout time.Duration
 
 func DefineKubeClientFlags(cmd *kingpin.CmdClause) {
+	// Settings for Kubernetes connection.
 	cmd.Flag("kube-context", "The name of the kubeconfig context to use. Can be set with $KUBE_CONTEXT.").
 		Envar("KUBE_CONTEXT").
 		Default(KubeContext).
 		StringVar(&KubeContext)
-
 	cmd.Flag("kube-config", "Path to the kubeconfig file. Can be set with $KUBE_CONFIG.").
 		Envar("KUBE_CONFIG").
 		Default(KubeConfig).
 		StringVar(&KubeConfig)
-
-	// Rate limit settings for kube client
-	cmd.Flag("kube-client-qps", "QPS for a rate limiter of a kubernetes client. Can be set with $KUBE_CLIENT_QPS.").
-		Envar("KUBE_CLIENT_QPS").
-		Default(KubeClientQpsDefault).
-		Float32Var(&KubeClientQps)
-	cmd.Flag("kube-client-burst", "Burst for a rate limiter of a kubernetes client. Can be set with $KUBE_CLIENT_BURST.").
-		Envar("KUBE_CLIENT_BURST").
-		Default(KubeClientBurstDefault).
-		IntVar(&KubeClientBurst)
-
-	cmd.Flag("kube-client-timeout", "Timeout for each request to the Kubernetes API server. Can be set with $KUBE_CLIENT_TIMEOUT").
-		Envar("KUBE_CLIENT_TIMEOUT").
-		Default(KubeClientTimeoutDefault).
-		DurationVar(&KubeClientTimeout)
-
 	cmd.Flag("kube-server", "The address and port of the Kubernetes API server. Can be set with $KUBE_SERVER.").
 		Envar("KUBE_SERVER").
 		Default(KubeServer).
 		StringVar(&KubeServer)
+
+	// Rate limit settings for 'main' kube client
+	cmd.Flag("kube-client-qps", "QPS for a rate limiter of a Kubernetes client for hook events. Can be set with $KUBE_CLIENT_QPS.").
+		Envar("KUBE_CLIENT_QPS").
+		Default(KubeClientQpsDefault).
+		Float32Var(&KubeClientQps)
+	cmd.Flag("kube-client-burst", "Burst for a rate limiter of a Kubernetes client for hook events. Can be set with $KUBE_CLIENT_BURST.").
+		Envar("KUBE_CLIENT_BURST").
+		Default(KubeClientBurstDefault).
+		IntVar(&KubeClientBurst)
+
+	// Settings for 'object_patcher' kube client
+	cmd.Flag("object-patcher-kube-client-qps", "QPS for a rate limiter of a Kubernetes client for Object patcher. Can be set with $OBJECT_PATCHER_KUBE_CLIENT_QPS.").
+		Envar("OBJECT_PATCHER_KUBE_CLIENT_QPS").
+		Default(ObjectPatcherKubeClientQpsDefault).
+		Float32Var(&ObjectPatcherKubeClientQps)
+	cmd.Flag("object-patcher-kube-client-burst", "Burst for a rate limiter of a Kubernetes client for Object patcher. Can be set with $OBJECT_PATCHER_KUBE_CLIENT_BURST.").
+		Envar("OBJECT_PATCHER_KUBE_CLIENT_BURST").
+		Default(ObjectPatcherKubeClientBurstDefault).
+		IntVar(&ObjectPatcherKubeClientBurst)
+	cmd.Flag("object-patcher-kube-client-timeout", "Timeout for object patcher requests to the Kubernetes API server. Can be set with $OBJECT_PATCHER_KUBE_CLIENT_TIMEOUT").
+		Envar("OBJECT_PATCHER_KUBE_CLIENT_TIMEOUT").
+		Default(ObjectPatcherKubeClientTimeoutDefault).
+		DurationVar(&ObjectPatcherKubeClientTimeout)
+
 }
