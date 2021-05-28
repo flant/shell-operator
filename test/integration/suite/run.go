736bf7d9db29cf253b1bda3b6884f84a4f53ffc1
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2021-05-28 18:22:58 +0300

    fix integration test usage

--- test/integration/suite/run.go
+++ test/integration/suite/run.go
@@ -9,10 +9,8 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"github.com/flant/shell-operator/pkg/kube/object_patch"
-	. "github.com/flant/shell-operator/test/utils"
-
 	klient "github.com/flant/kube-client/client"
+	"github.com/flant/shell-operator/pkg/kube/object_patch"
 )
 
 var (
@@ -36,7 +34,7 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 }, func([]byte) {
 	// Initialize kube client out-of-cluster
 	ContextName = KindGetKubeContext(ClusterName)
-	KubeClient = kube.NewKubernetesClient()
+	KubeClient = klient.New()
 	KubeClient.WithContextName(ContextName)
 	err := KubeClient.Init()
 	Expect(err).ShouldNot(HaveOccurred())
