34a6641ac64b8bdb7b295e5a4c7f6755c8b1989d
Yuriy Losev <yuriy.losev@flant.com>
2023-06-17 00:26:12 +0400

    Add format for `queue list` command

--- pkg/task/dump/dump.go
+++ pkg/task/dump/dump.go
@@ -1,31 +1,34 @@
 package dump
 
 import (
+	"encoding/json"
 	"fmt"
 	"sort"
 	"strings"
+
+	"sigs.k8s.io/yaml"
 
 	"github.com/flant/shell-operator/pkg/task"
 	"github.com/flant/shell-operator/pkg/task/queue"
 )
 
-// AsQueueNames implements sort.Interface for array of queue names.
+// asQueueNames implements sort.Interface for array of queue names.
 // 'main' queue has the top priority. Other names are sorted as usual.
-type AsQueueNames []string
+type asQueueNames []dumpQueue
 
-func (a AsQueueNames) Len() int      { return len(a) }
-func (a AsQueueNames) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a AsQueueNames) Less(i, j int) bool {
+func (a asQueueNames) Len() int      { return len(a) }
+func (a asQueueNames) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a asQueueNames) Less(i, j int) bool {
 	p, q := a[i], a[j]
 
 	switch {
-	case q == queue.MainQueueName:
+	case q.Name == queue.MainQueueName:
 		return false
-	case p == queue.MainQueueName:
+	case p.Name == queue.MainQueueName:
 		return true
 	}
 	// both names are main or both are not main, so compare as usual.
-	return p < q
+	return p.Name < q.Name
 }
 
 // TaskQueueMainToText dumps only the 'main' queue.
@@ -42,103 +45,85 @@ func TaskQueueMainToText(tqs *queue.TaskQueueSet) string {
 	return buf.String()
 }
 
-// TaskQueueSetToText dumps all queues.
-func TaskQueueSetToText(tqs *queue.TaskQueueSet) string {
-	var buf strings.Builder
-
-	names := map[string][]string{
-		"active": make([]string, 0),
-		"empty":  make([]string, 0),
+// TaskQueues dumps all queues.
+func TaskQueues(tqs *queue.TaskQueueSet, format string, showEmpty bool) string {
+	//return TaskQueueSetToText(tqs, showEmpty)
+	result := dumpTaskQueues{
+		Empty:  make([]dumpQueue, 0),
+		Active: make([]dumpQueue, 0),
 	}
+
 	otherQueuesCount := 0
 	activeQueues := 0
 	emptyQueues := 0
 	tasksCount := 0
 	mainTasksCount := 0
-	hasMain := false
-	hasOthers := false
+
 	tqs.Iterate(func(queue *queue.TaskQueue) {
+		if queue == nil {
+			return
+		}
+
 		if queue.Name == tqs.MainName {
 			mainTasksCount = queue.Length()
-			hasMain = true
 			if queue.IsEmpty() {
-				names["empty"] = append(names["empty"], queue.Name)
+				mainQueue := dumpQueue{
+					Name:       queue.Name,
+					TasksCount: queue.Length(),
+				}
+				result.Empty = append(result.Empty, mainQueue)
+				result.MainQueue = &mainQueue
 			} else {
-				names["active"] = append(names["active"], queue.Name)
+				tasks := getTasksForQueue(queue)
+				mainQueue := dumpQueue{
+					Name:       queue.Name,
+					TasksCount: queue.Length(),
+					Status:     queue.Status,
+					Tasks:      tasks,
+				}
+				result.Active = append(result.Active, mainQueue)
+				result.MainQueue = &mainQueue
 			}
 			return
 		}
 
 		otherQueuesCount++
-		hasOthers = true
 		if queue.IsEmpty() {
 			emptyQueues++
-			names["empty"] = append(names["empty"], queue.Name)
+			result.Empty = append(result.Empty, dumpQueue{
+				Name: queue.Name,
+			})
 		} else {
 			activeQueues++
 			tasksCount += queue.Length()
-			names["active"] = append(names["active"], queue.Name)
+			tasks := getTasksForQueue(queue)
+			result.Active = append(result.Active, dumpQueue{
+				Name:       queue.Name,
+				TasksCount: queue.Length(),
+				Status:     queue.Status,
+				Tasks:      tasks,
+			})
 		}
 	})
 
-	sort.Sort(AsQueueNames(names["active"]))
-	sort.Sort(AsQueueNames(names["empty"]))
+	result.SortByName()
 
-	for i, name := range names["active"] {
-		q := tqs.GetByName(name)
-		if i > 0 {
-			buf.WriteString("\n")
-		}
-		if q == nil {
-			buf.WriteString(fmt.Sprintf("Queue '%s' is not created\n", name))
-		} else {
-			buf.WriteString(TaskQueueToText(q))
+	result.Summary = dumpSummary{
+		mainQueueTasksCount:    mainTasksCount,
+		otherQueuesActiveCount: activeQueues,
+		otherQueuesEmptyCount:  emptyQueues,
+		otherQueuesTasksCount:  tasksCount,
+		totalTasksCount:        mainTasksCount + tasksCount,
+	}
+
+	if showEmpty {
+		// Empty queues. Do not report single empty main queue.
+		if len(result.Empty) > 0 && otherQueuesCount == 0 {
+			showEmpty = false
 		}
 	}
 
-	// Empty queues. Do not report single empty main queue.
-	if emptyQueues > 0 && hasOthers {
-		buf.WriteString(fmt.Sprintf("\nEmpty queues (%d):\n", len(names["empty"])))
-		for _, name := range names["empty"] {
-			buf.WriteString(fmt.Sprintf("- %s\n", name))
-		}
-	}
-
-	// Summary:
-	//   No queues.
-	// or
-	// Summary:
-	// - 'main' queue: [empty | 1 task | 5 tasks].
-	// - 24 other queues (3 active, 21 empty): 56 tasks.
-	// - total 68 tasks to handle.
-	if !hasMain && !hasOthers {
-		buf.WriteString("Summary:\n  No queues.\n")
-	} else {
-		if buf.Len() > 0 {
-			buf.WriteString("\n")
-		}
-		buf.WriteString("Summary:\n")
-		if hasMain {
-			buf.WriteString(fmt.Sprintf("- '%s' queue: %s.\n",
-				tqs.MainName,
-				pluralize(mainTasksCount, "empty", "task", "tasks")))
-		}
-		if hasOthers {
-			buf.WriteString(fmt.Sprintf("- %s (%d active, %d empty): %s.\n",
-				pluralize(otherQueuesCount, "", "other queue", "other queues"),
-				activeQueues, emptyQueues,
-				pluralize(tasksCount, "", "task", "tasks")))
-		}
-		totalTasks := mainTasksCount + tasksCount
-		if totalTasks == 0 {
-			buf.WriteString("- no tasks to handle.\n")
-		} else {
-			buf.WriteString(fmt.Sprintf("- total %s to handle.\n",
-				pluralize(totalTasks, "", "task", "tasks")))
-		}
-	}
-
-	return buf.String()
+	return result.Format(format, showEmpty)
 }
 
 func pluralize(n int, zero, one, many string) string {
@@ -165,6 +150,171 @@ func TaskQueueToText(q *queue.TaskQueue) string {
 		buf.WriteString("\n")
 		index++
 	})
+
+	return buf.String()
+}
+
+func getTasksForQueue(q *queue.TaskQueue) []dumpTask {
+	tasks := make([]dumpTask, 0, q.Length())
+
+	index := 1
+	q.Iterate(func(task task.Task) {
+		tasks = append(tasks, dumpTask{
+			Index:       index,
+			Description: task.GetDescription(),
+		})
+		index++
+	})
+
+	return tasks
+}
+
+type dumpTaskQueues struct {
+	MainQueue *dumpQueue  `json:"-"`
+	Active    []dumpQueue `json:"active" yaml:"active"`
+	Empty     []dumpQueue `json:"empty,omitempty" yaml:"empty,omitempty"`
+
+	Summary dumpSummary `json:"summary" yaml:"summary"`
+}
+
+type dumpQueue struct {
+	Name       string     `json:"name"`
+	TasksCount int        `json:"tasksCount"`
+	Status     string     `json:"status,omitempty"`
+	Tasks      []dumpTask `json:"tasks,omitempty"`
+}
+
+type dumpTask struct {
+	Index       int    `json:"index"`
+	Description string `json:"description"`
+}
+
+type dumpSummary struct {
+	// - 'main' queue: [empty | 1 task | 5 tasks].
+	// - 24 other queues (3 active, 21 empty): 56 tasks.
+	// - total 68 tasks to handle.
+
+	mainQueueTasksCount    int
+	otherQueuesActiveCount int
+	otherQueuesEmptyCount  int
+	otherQueuesTasksCount  int
+	totalTasksCount        int
+}
+
+func (ds dumpSummary) MarshalJSON() ([]byte, error) {
+	s := struct {
+		Main   int `json:"mainQueueTasks"`
+		Others struct {
+			Active int `json:"active"`
+			Empty  int `json:"empty"`
+			Tasks  int `json:"tasks"`
+		} `json:"otherQueues"`
+		TotalTasks int `json:"totalTasks"`
+	}{
+		Main:       ds.mainQueueTasksCount,
+		TotalTasks: ds.totalTasksCount,
+	}
+
+	s.Others.Tasks = ds.otherQueuesTasksCount
+	s.Others.Active = ds.otherQueuesActiveCount
+	s.Others.Empty = ds.otherQueuesEmptyCount
+
+	return json.Marshal(s)
+}
+
+func (dtq dumpTaskQueues) SortByName() {
+	sort.Sort(asQueueNames(dtq.Active))
+
+	sort.Sort(asQueueNames(dtq.Empty))
+}
+
+func (dtq dumpTaskQueues) Format(format string, showEmpty bool) string {
+	switch format {
+	case "text":
+		return dtq.asText(showEmpty)
+
+	case "json":
+		if !showEmpty {
+			dtq.Empty = make([]dumpQueue, 0)
+		}
+		res, _ := json.Marshal(dtq)
+
+		return string(res)
+
+	case "yaml":
+		if !showEmpty {
+			dtq.Empty = make([]dumpQueue, 0)
+		}
+		res, _ := yaml.Marshal(dtq)
+
+		return string(res)
+	}
+
+	return ""
+}
+
+func (dtq dumpTaskQueues) asText(showEmpty bool) string {
+	var buf strings.Builder
+
+	for i, taskQueue := range dtq.Active {
+		if i > 0 {
+			buf.WriteString("\n")
+		}
+
+		buf.WriteString(fmt.Sprintf("Queue '%s': length %d, status: '%s'\n", taskQueue.Name, taskQueue.TasksCount, taskQueue.Status))
+		buf.WriteString("\n")
+
+		for _, tk := range taskQueue.Tasks {
+			buf.WriteString(fmt.Sprintf("%2d. ", tk.Index))
+			buf.WriteString(tk.Description)
+			buf.WriteString("\n")
+		}
+	}
+
+	// Empty queues. Do not report single empty main queue.
+	if showEmpty {
+		buf.WriteString(fmt.Sprintf("\nEmpty queues (%d):\n", len(dtq.Empty)))
+		for _, taskQueue := range dtq.Empty {
+			buf.WriteString(fmt.Sprintf("- %s\n", taskQueue.Name))
+		}
+	}
+
+	// Summary:
+	//   No queues.
+	// or
+	// Summary:
+	// - 'main' queue: [empty | 1 task | 5 tasks].
+	// - 24 other queues (3 active, 21 empty): 56 tasks.
+	// - total 68 tasks to handle.
+	if len(dtq.Active) == 0 && len(dtq.Empty) == 0 {
+		buf.WriteString("Summary:\n  No queues.\n")
+	} else {
+		otherQueuesCount := len(dtq.Active) + len(dtq.Empty)
+
+		if buf.Len() > 0 {
+			buf.WriteString("\n")
+		}
+		buf.WriteString("Summary:\n")
+		if dtq.MainQueue != nil {
+			otherQueuesCount -= 1 // minus main queue
+			buf.WriteString(fmt.Sprintf("- '%s' queue: %s.\n",
+				dtq.MainQueue.Name,
+				pluralize(len(dtq.MainQueue.Tasks), "empty", "task", "tasks")))
+		}
+
+		if otherQueuesCount > 0 {
+			buf.WriteString(fmt.Sprintf("- %s (%d active, %d empty): %s.\n",
+				pluralize(otherQueuesCount, "", "other queue", "other queues"),
+				dtq.Summary.otherQueuesActiveCount, dtq.Summary.otherQueuesEmptyCount,
+				pluralize(dtq.Summary.otherQueuesTasksCount, "", "task", "tasks")))
+		}
+		if dtq.Summary.totalTasksCount == 0 {
+			buf.WriteString("- no tasks to handle.\n")
+		} else {
+			buf.WriteString(fmt.Sprintf("- total %s to handle.\n",
+				pluralize(dtq.Summary.totalTasksCount, "", "task", "tasks")))
+		}
+	}
 
 	return buf.String()
 }
