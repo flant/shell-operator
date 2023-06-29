fff6411e6a6e823f732bba1e2d9c241b919efd4f
Yuriy Losev <yuriy.losev@flant.com>
2023-06-29 15:29:00 +0400

    ++

--- pkg/kube_events_manager/filter_test.go
+++ pkg/kube_events_manager/filter_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
--- test/integration/kubeclient/kube_client_test.go
+++ test/integration/kubeclient/kube_client_test.go
@@ -7,10 +7,11 @@ import (
 	"context"
 	"testing"
 
-	. "github.com/flant/shell-operator/test/integration/suite"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	. "github.com/flant/shell-operator/test/integration/suite"
 )
 
 func Test(t *testing.T) {
